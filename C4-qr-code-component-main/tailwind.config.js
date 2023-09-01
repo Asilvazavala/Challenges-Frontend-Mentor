dd9393635ecccaf1e100853edf34fd6f1dd35b14
Asilvazavala <asilvazavala@gmail.com>
2023-09-01 12:37:17 -0600

    added challenge 4

---
+++ C4-qr-code-component-main/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        LightGrey: 'rgb(213, 225, 239)',
+        StrongBlue: '#212E47',
+        Grey: '#9497A1'
+      }
+    },
+  },
+  plugins: [],
+}
+
